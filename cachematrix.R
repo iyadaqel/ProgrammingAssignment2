a50bf40f4574c87b6c36b83327b3363a394a51a5
Iyad Al Aqel <iyad.aqel1@gmail.com>
2014-05-25 20:13:22 +0300

    PA2 assingment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+#This function is an efficient way of computing the inverse of the matrix by caching the inverse.
+#if the matrix doesn't change. 
 
-## Write a short comment describing this function
+## This function will create the new type of Matrix and its a list of four things. Setting the matrix
+#getting the matrix , setting the inverse and getting the inverse.
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+i <- NULL 
+set <- function(y){
+  x <<- y 
+  i <<- NULL
+}
+get <- function()x
+setInverse <- function(inverse) i <<- inverse
+getInverse <- function()i 
+list(set=set , get=get , setInverse = setInverse , getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## This function will be used to compute the inverse of the matrix and cache it if its null.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  i <- x$getInverse()
+  if(!is.null(i)){
+    return(i)
+  }
+  m <- x$get()
+  i <- solve(m)
+  x$setInverse(i)
+  i 
 }
